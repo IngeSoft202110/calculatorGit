6b59702dcea1f21c478554ce738c35ec84a064ee
Julian Camilo Builes Serrano <buiser147@hotmail.com>
2021-02-22 18:28:50 -0500

    Correccion de errores

--- src/co/edu/javeriana/calculator/CalculatorJCBS.java
+++ src/co/edu/javeriana/calculator/CalculatorJCBS.java
@@ -3,24 +3,27 @@ package co.edu.javeriana.calculator;
 public class CalculatorJCBS implements ICalculator{
 
 	@Override
-	public double add(double a, double b) {
-		return a+b;
-	}
-	@Override
-	public double substract (double a, double b) {
-		return a-b;
-	}
-	@Override
-	public double multiply (double a, double b) {
-		return a*b;
-	}
-	@Override
-	public double divide (double a, double b) throws ArithmeticException{
-		return a/b;
-	}
-	
-	@Override
-	public String about() {
-		return "Julian Camilo Builes Serrano";
-	}
+    public double add(double a, double b) {
+        return (a+b);
+    }
+
+    @Override
+    public double substract(double a, double b) {
+        return (a-b);
+    }
+
+    @Override
+    public double multiply(double a, double b) {
+         return (a*b);
+    }
+
+    @Override
+    public double divide(double a, double b) throws ArithmeticException {
+        return(a/b);
+    }
+
+    @Override
+    public String about() {
+        return ("Julian Camilo Builes Serrano");
+    }
 }
