e12ac8c80f13a3a2b6cb7155246fb1e9ad6a1f38
Santiago Bermudez <ds.bermudezr@javeriana.edu.co>
2021-02-23 23:33:53 -0500

    Cambio por Santiago Bermudez

---
+++ src/co/edu/javeriana/calculator/CalculatorDSBR.java
@@ -0,0 +1,35 @@
+package co.edu.javeriana.calculator;
+
+public interface CalculatorDSBR {
+	@Override
+	
+	public double add(double a, double b)
+	{
+		return (a+b);
+	}
+	@Override
+	public double substract (double a, double b)
+	{
+		return (a-b);
+	}
+	@Override
+	public double multiply (double a, double b)
+	{
+		return (a*b);
+	}
+	@Override
+	public double divide (double a, double b) throws ArithmeticException
+	{
+		if(b == 0)
+		{
+			throw new ArithmeticException("Error, el divisor tiene que ser diferente a 0");
+		}
+		return(a/b);
+	}
+	
+	public String about()
+	{
+		return Authors.DSBR;
+	}
+
+}
