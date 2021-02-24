9b71ecc1a75982ee716754cd2506ab6fe5f0f6c7
juan-hamon <53021906+juan-hamon@users.noreply.github.com>
2021-02-23 19:44:58 -0500

    Taller git Juan Francisco Hamon

--- src/co/edu/javeriana/calculator/Authors.java
+++ src/co/edu/javeriana/calculator/Authors.java
@@ -7,5 +7,6 @@ public class Authors {
 	public static String VRB = "Valentina Rozo Bernal";
 	public static String JDCN = "Juan Diego Campos Neira"; 
 	public static String JCBS ="Julian Camilo Builes Serrano";
-	public static String SDS = "Simón Dávila Saravia";
+	public static String SDS = "Simï¿½n Dï¿½vila Saravia";
+	public static String JFH = "Juan Francisco Hamon";
 }
